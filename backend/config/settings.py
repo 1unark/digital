3a26e036d85101ce46765b23cf30647e58fcddcc
1unark <157058852+1unark@users.noreply.github.com>
2026-01-11 11:44:49 -0800

    whitenoise

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'comments',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -40,6 +42,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
